1905be08c3ee09807eb0691c020b74f28c6d0ae3
Larry <ltt28@cornell.edu>
2024-12-02 17:23:54 -0500

    nme deliverable solution (no extra cred)

--- demo/urls.py
+++ demo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('api/users/', views.user_route, name='users'),
+    path('api/users/<int:id>/', views.get_user_by_id, name='get_user'),
     path('api/users/transactions/', views.transaction_route, name='transactions')
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import Http404, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from demo.models import User
@@ -13,9 +13,33 @@ def user_route(request):
         serializer = UserSerializer(users, many=True)
         return JsonResponse(serializer.data, safe=False)
     if request.method == 'POST':
-        # TODO: Implement creating a new user
-        pass
+        try:
+            data = JSONParser().parse(request)
+        except Exception as e:
+            return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+        name = data.get('name')
+        username = data.get('username')
+        balance = data.get('balance')
+
+        if not name or not username or balance is None:
+            return JsonResponse({'error': 'Missing required fields'}, status=400)
+        
+        user = User.objects.create(name=name, username=username, balance=balance)
+        serializer = UserSerializer(user, many=False)
+        return JsonResponse(serializer.data, safe=False, status=201)
     
+@csrf_exempt
+def get_user_by_id(request, id):
+    if request.method == 'GET':
+        try:
+            user = User.objects.get(id=id)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'User Not Found'}, status=404)
+        serializer = UserSerializer(user, many=False)
+        return JsonResponse(serializer.data, safe=False, status=200)
+        
+
+
 @csrf_exempt
 def transaction_route(request):
     # TODO: EXTRA CREDIT: Get all of a user's transactions
